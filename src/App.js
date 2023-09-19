f3b5fc18b65c85cf4367f32a7f48a6a7801c4fa9
Isabellanamutenda <namtendaisabella@gmail.com>
2023-09-19 08:47:43 +0300

    Add boostrap routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 // 👇️ import Routes instead of Switch 👇️
 import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 {
   /* The following line can be included in your src/index.js or App.js file */
 }
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (

50a5a0db69f56ad39393e6b004b9947636efc6d2
Isabellanamutenda <namtendaisabella@gmail.com>
2023-09-20 23:09:51 +0300

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,62 +1,83 @@
 import React from 'react';
 // 👇️ import Routes instead of Switch 👇️
 import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-{
-  /* The following line can be included in your src/index.js or App.js file */
-}
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <Router>
+//       <div>
+//         <nav>
+//           <ul>
+//             <li>
+//               <Link to="/">Home</Link>
+//             </li>
+//             <li>
+//               <Link to="/about">About</Link>
+//             </li>
+//             <li>
+//               <Link to="/projects">Projects</Link>
+//             </li>
+//             <li>
+//               <Link to="/skills">Skills</Link>
+//             </li>
+//             <li>
+//               <Link to="/contact">Contact</Link>
+//             </li>
+//           </ul>
+//         </nav>
+
+//         {/* 👇️ Wrap your Route components in a Routes component */}
+//         <Routes>
+//           <Route path="/about" element={<About />} />
+//           <Route path="/" element={<Home />} />
+//           <Route path="/projects" element={<Projects />} />
+//           <Route path="/skills" element={<Skills />} />
+//           <Route path="/contact" element={<Contact />} />
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// function Home() {
+//   return <h2>Home</h2>;
+// }
+
+// function About() {
+//   return <h2>About</h2>;
+// }
+// function Projects() {
+//   return <h2>Projects</h2>;
+// }
+// function Skills() {
+//   return <h2>Skills</h2>;
+// }
+// function Contact() {
+//   return <h2>Contact</h2>;
+// }
+
+
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Experience from "./components/Experience";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Portfolio from "./components/Portfolio";
+import SocialLinks from "./components/SocialLinks";
+
+function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>
-            <li>
-              <Link to="/skills">Skills</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+      <Portfolio />
+      <Experience />
+      <Contact />
 
-        {/* 👇️ Wrap your Route components in a Routes component */}
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
+      <SocialLinks />
+    </div>
   );
 }
 
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-function Projects() {
-  return <h2>Projects</h2>;
-}
-function Skills() {
-  return <h2>Skills</h2>;
-}
-function Contact() {
-  return <h2>Contact</h2>;
-}
-
+export default App;

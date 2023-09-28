3d4894c4171634c5640bb97a92d69b609efe928e
Isabellanamutenda <namtendaisabella@gmail.com>
2023-09-28 10:58:56 +0300

    Add changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Experience from "./components/Experience";
@@ -6,17 +6,17 @@ import Home from "./components/Home";
 import Header from "./components/Header";
 
 function App() {
+  	// Element type is invalid: expected a string (for built-in components) 
+	// or a class/function (for composite components) but got: object.
   return (
 
     <div>
-
     <Header />
     <Home />
     <About />
     <Experience />
     <Contact />
 
-    
   </div>
   );
 }
